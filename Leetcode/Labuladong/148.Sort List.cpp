aff9e49382b754f7785a907eeaeab1c14fc5e8d4
Xzz13 <18532110501@163.com>
2024-10-31 21:06:53 +0800

    lianbiao

---
+++ Leetcode/Labuladong/148.Sort List.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=cpp
+ * @lcpr version=20002
+ *
+ * [148] 排序链表
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *sortList(ListNode *head)
+    {
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [4,2,1,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-1,5,3,4,0]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
